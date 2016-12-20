64f62e2914caefd904c169a65725059340971fb3
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-20 01:10:09 -0800

    refactor server to run on express

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const http = require('http');
-const BEV = require('./model/bevs.js');
-const Router = require('./lib/router.js');
-const storage = require('./lib/storage.js');
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const jsonParser = require('body-parser');
+const debug = require('debug')('bev:server');
+
+const app = express();
+const BEV = require('./model/bev.js');
+
 const PORT = 3000;
-const router = new Router();
 
-require('./route/bev-route.js')(router);
-
-const server = http.createServer(router.route());
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
