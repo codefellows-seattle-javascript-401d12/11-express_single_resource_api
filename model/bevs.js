41b888db09b5ff87f21c7c1ba6a11b829b77f2eb
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-20 02:09:54 -0800

    refactor for express, debug issues with new range property

--- model/bevs.js
+++ model/bevs.js
@@ -3,10 +3,17 @@
 // Model for data on Battery-Electric Vehicles (BEVs)
 
 const uuid = require('node-uuid');
+const createError = require('http-errors');
+const debug = require('debug')('bev:bev');
+const storage = require('../lib/storage.js');
 
-module.exports = function(vehicle, info) {
+const BEV = module.exports = function(vehicle, info, range) {
+  debug('BEV constructor');
+
   if (!vehicle) throw new Error('expected vehicle model name');
   if (!info) throw new Error('expected vehicle model info');
+  if (!range) throw new Error('expected vehicle model range');
+  if (isNaN(range)) throw new Error('expected range to be a number');
 
   this.id = uuid.v1();
   // vehicle make, model
@@ -14,5 +21,17 @@ module.exports = function(vehicle, info) {
   // general vehicle description
   this.info = info;
   // EPA-tested range on a single battery charge, in miles
+  // range must be a number
   this.range = range;
+};
+
+BEV.createVehicle = function(_vehicle) {
+  debug('createVehicle');
+
+  try {
+    let bev = new BEV(_vehicle.vehicle, _vehicle.info, _vehicle.range);
+    return storage.createEntry('bev', bev);
+  } catch (err) {
+    return Promise.reject(err);
+  };
 };
