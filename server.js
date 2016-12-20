53964f3da59a8979aeb2056971938f58ed4b0c9d
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-19 18:50:20 -0800

    set up server.js server up and running as expected. No routes have been added yet.

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const debug = require('debug')('ski:server');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const createError = require('http-errors');
+const parseJson = require('body-parser').json();
+const PORT = process.env.PORT || 3000
+
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log(`Server up: ${PORT}`);
+});
