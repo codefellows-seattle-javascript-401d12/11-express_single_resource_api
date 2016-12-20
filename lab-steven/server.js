09aa800aea39c79d622484ba74a56e6b6e254c23
Steven Bateman <batemanvo@gmail.com>
2016-12-19 18:36:09 -0800

    Moved old router functionality. Changed previous router.get method to app.get call within server.js.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -2,9 +2,22 @@
 
 const express = require('express');
 const parseJSON = require('body-parser').json();
+const storage = require('./lib/storage.js');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+});
+
+app.get('/api/student', (request, response, next) => {
+  if (request.query.id) {
+    storage.getItem('student', request.query.id)
+    .then(student => response.json(student))
+    .catch(err => next(err));
+    return;
+  }
+  storage.getAllItems('student')
+  .then(allIds => response.json(allIds))
+  .catch(err => next(err));
 });
