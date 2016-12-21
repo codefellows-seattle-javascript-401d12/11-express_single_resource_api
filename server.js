b68e4aff19b178a9260361b55cf1386c98fd2a29
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-20 18:02:24 -0800

    build out GET routes for retrieving single entries or all entries

--- server.js
+++ server.js
@@ -21,6 +21,22 @@ app.post('/api/bev', jsonParser, function(req, res, next) {
   .catch( err => next(err));
 });
 
+app.get('/api/bev', function(req, res, next) {
+  debug('GET: api/bev');
+
+  if (req.query.id) {
+    BEV.fetchVehicle(req.query.id)
+    .then( vehicle => res.json(vehicle))
+    .catch( err => next(err));
+  };
+
+  if (!req.query.id) {
+    BEV.fetchAllVehicles()
+    .then( vehicles => res.json(vehicles))
+    .catch( err => next(err));
+  };
+});
+
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
